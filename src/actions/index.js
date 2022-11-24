59225cbc176d99cfb3e06b5fe9a6a733f943fe5b
EmilS <emil.s21165@gmail.com>
2022-11-24 12:44:16 +0600

    added ReduxThunk to store and changed requests in HeroesList, HeroesFilters components

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching);
+        request("http://localhost:3001/heroes")
+            .then(data => dispatch(heroesFetched(data)))
+            .catch(() => dispatch(heroesFetchingError()))
+}
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -15,6 +22,13 @@ export const heroesFetchingError = () => {
     return {
         type: 'HEROES_FETCHING_ERROR'
     }
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+        request("http://localhost:3001/filters")
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
 }
 
 export const filtersFetching = () => {
@@ -38,9 +52,9 @@ export const filtersFetchingError = () => {
 
 export const activeFilterChanged = (filter) => {
     return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
+            type: 'ACTIVE_FILTER_CHANGED',
+            payload: filter
+        }
 }
 
 export const heroCreated = (hero) => {
--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,7 +2,7 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
-import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
+import { fetchHeroes, heroDeleted } from '../../actions';
 import { createSelector } from 'reselect';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
@@ -28,11 +28,8 @@ const HeroesList = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(heroesFetching);
-        request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
-
+        dispatch(fetchHeroes(request));
+       
         // eslint-disable-next-line
     }, []);
 
